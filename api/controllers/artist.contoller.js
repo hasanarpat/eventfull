4796f5457c55e36367265571bf4858d117d9f24f
hasanarpat <black_the_dragon@hotmail.com>
2024-05-02 20:40:28 +0300

    create data base models

---
+++ api/controllers/artist.contoller.js
@@ -0,0 +1,11 @@
+export const getArtist = async (req, res) => {
+  const id = req.params.id;
+};
+
+export const getArtists = async (req, res) => {};
+
+export const addArtist = async (req, res) => {};
+
+export const updateArtist = async (req, res) => {};
+
+export const deleteArtist = async (req, res) => {};
--- api/routes/artist.route.js
+++ api/routes/artist.route.js
@@ -1,5 +1,18 @@
 import express from 'express';
+import {
+  addArtist,
+  deleteArtist,
+  getArtist,
+  getArtists,
+  updateArtist,
+} from '../controllers/artist.contoller.js';
 
 const artistRoute = express.Router();
+
+artistRoute.get('/artists', getArtists);
+artistRoute.get('/:id', getArtist);
+artistRoute.post('/:id', addArtist);
+artistRoute.put('/:id', updateArtist);
+artistRoute.delete('/:id', deleteArtist);
 
 export default artistRoute;

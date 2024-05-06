040a5cfd2c1aa84bc45bdd5738c21ba10eea080a
hasanarpat <black_the_dragon@hotmail.com>
2024-05-06 18:37:10 +0300

    finish footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,7 +1,59 @@
 import styles from './footer.module.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.innerWrapper}>
+            <div>
+              <h4>Events</h4>
+              <ul>
+                <li>Find Near Events</li>
+                <li>Incoming Events</li>
+                <li>Get Notifications</li>
+              </ul>
+            </div>
+            <div>
+              <h4>Artists</h4>
+              <ul>
+                <li>Popular Artists</li>
+                <li>Rising Artists</li>
+                <li></li>
+              </ul>
+            </div>
+            <div>
+              <h4>Contact Us</h4>
+              <ul>
+                <li>Career</li>
+                <li>Blog</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+            <div>
+              <h4>Privacy</h4>
+              <ul>
+                <li>Help</li>
+                <li>Delete Account</li>
+                <li>Copyright</li>
+                <li>Cookies</li>
+              </ul>
+            </div>
+            <div>
+              <Link to='/'>
+                <img src='/assets/logo-dark.jpg' alt='' />
+              </Link>
+            </div>
+          </div>
+          <span>
+            All rights are reserved. 2024{' '}
+            <b>Eventfull Entertainment and Software Inc</b>. ©
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

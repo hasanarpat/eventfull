0d44a28cdcb9ea55d7e3438e8d0de1ba33a67279
hasanarpat <black_the_dragon@hotmail.com>
2024-05-07 15:44:21 +0300

    finish hero section design

--- client/src/routes/home/Home.jsx
+++ client/src/routes/home/Home.jsx
@@ -1,54 +1,71 @@
 import styles from './home.module.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <section className={styles.home}>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <div className={styles.sample}>
-        <div className={styles.sampleInner}>
-          <button>merhaba</button>
-          <span>Accent</span>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.hero}>
+            <div className={styles.bgOpacity} />
+            <div className={styles.texts}>
+              <div className={styles.top}>
+                <h1>
+                  GET IN TOUCH WITH NEW PEOPLE AND CULTURE WITH FULL OF EVENTS
+                  OF EVENTFULL
+                </h1>
+                <h3>
+                  Ullamco labore laboris adipisicing qui tempor magna id magna
+                  dolor exercitation dolore mollit.
+                </h3>
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.buttons}>
+                  <button>
+                    <Link to='/events'>Explore Near Events</Link>
+                  </button>
+                  <button>
+                    <Link to='/artists'>Explore New Artists</Link>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
+          <h1>Eventfull</h1>
         </div>
       </div>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
-      <h1>Eventfull</h1>
     </section>
   );
 };

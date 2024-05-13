113ba59d0ca56c55cefb4906299410bebc1dee33
hasanarpat <black_the_dragon@hotmail.com>
2024-05-13 18:44:16 +0300

    finish artists and events pages structure with ready to go components

--- client/src/routes/artists/Artists.jsx
+++ client/src/routes/artists/Artists.jsx
@@ -1,7 +1,22 @@
+import Datagrid from '../../components/datagrid/Datagrid';
+import Popular from '../../components/popular/Popular';
+import Search from '../../components/search/Search';
+import Tickets from '../../components/tickets/Tickets';
 import styles from './artists.module.css';
 
 const Artists = () => {
-  return <section className={styles.artists}>Artists</section>;
+  return (
+    <section className={styles.artists}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <Search type='artist' />
+          <Tickets />
+          <Popular />
+          <Datagrid />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Artists;
--- client/src/routes/events/Events.jsx
+++ client/src/routes/events/Events.jsx
@@ -1,7 +1,22 @@
 import styles from './events.module.css';
+import Datagrid from '../../components/datagrid/Datagrid';
+import Popular from '../../components/popular/Popular';
+import Search from '../../components/search/Search';
+import Tickets from '../../components/tickets/Tickets';
 
 const Events = () => {
-  return <section className={styles.events}>Events</section>;
+  return (
+    <section className={styles.events}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <Search type='artist' />
+          <Tickets />
+          <Popular />
+          <Datagrid />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Events;

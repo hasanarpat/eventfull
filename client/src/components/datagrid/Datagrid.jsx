519cb90fe4bf7978ea0bdfda3dd78e0f786bfb26
hasanarpat <black_the_dragon@hotmail.com>
2024-05-13 18:37:52 +0300

    finish arena homepage and obtain elements as custom components to use again

---
+++ client/src/components/datagrid/Datagrid.jsx
@@ -0,0 +1,251 @@
+import styles from './datagrid.module.css';
+import Pagination from '../pagination/Pagination';
+import { Link } from 'react-router-dom';
+
+const Datagrid = () => {
+  return (
+    <section className={styles.datagrid}>
+      <p>All Arenas</p>
+      <div className={styles.datagridList}>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.In
+              commodo duis ullamco laborum ullamco ad. Aliqua Lorem proident qui
+              ut adipisicing irure eiusmod. Aute deserunt qui ullamco mollit
+              occaecat incididunt ipsum labore enim ad. Reprehenderit sunt quis
+              adipisicing occaecat irure. Dolore magna laboris nircitation qui
+              Lorem deserunt. Reprehenderit nisi.In commodo duis ullamco laborum
+              ullamco ad. Aliqua Lorem proident qui ut adipisicing irure
+              eiusmod. Aute deserunt qui ullamco mollit occaecat incididunt
+              ipsum labore enim ad. Reprehenderit sunt quis adipisicing occaecat
+              irure. Dolore magna laboris nircitation qui Lorem deserunt.
+              Reprehenderit nisi.In commodo duis ullamco laborum ullamco ad.
+              Aliqua Lorem proident qui ut adipisicing irure eiusmod. Aute
+              deserunt qui ullamco mollit occaecat incididunt ipsum labore enim
+              ad. Reprehenderit sunt quis adipisicing occaecat irure. Dolore
+              magna laboris nisi aliqua ipsum esse sit.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitrcitation qui Lorem deserunt. Reprehenderit nisi.In
+              commodo duis ullamco laborum ullamco ad. Aliqua Lorem proident qui
+              ut adipisicing irure eiusmod. Aute deserunt qui ullamco mollit
+              occaecat incididunt ipsum labore enim ad. Reprehenderit sunt quis
+              adipisicing occaecat irure. Dolore magna laboris nircitation qui
+              Lorem deserunt. Reprehenderit nisi.In commodo duis ullamco laborum
+              ullamco ad. Aliqua Lorem proident qui ut adipisicing irure
+              eiusmod. Aute deserunt qui ullamco mollit occaecat incididunt
+              ipsum labore enim ad. Reprehenderit sunt quis adipisicing occaecat
+              irure. Dolore magna laboris niation qui Lorem deserunt.
+              Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris ercitation qui Lorem deserunt. Reprehenderit
+              nisi.In commodo duis ullamco laborum ullamco ad. Aliqua Lorem
+              proident qui ut adipisicing irure eiusmod. Aute deserunt qui
+              ullamco mollit occaecat incididunt ipsum labore enim ad.
+              Reprehenderit sunt quis adipisicing occaecat irure. Dolore magna
+              laboris nist occaecat ipsum nulla enim esse ad magna labore
+              exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={`/blabla`} className={styles.datagridListItem}>
+          <img
+            src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='arena rock'
+          />
+          <div className={styles.datagridListItemTexts}>
+            <h4>Arena Rock</h4>
+            <p>
+              Consequat laboris est occaecat ipsum nulla enim esse ad magna
+              labore exercitation qui Lorem deserunt. Reprehenderit nisi.
+            </p>
+            <div className={styles.datagridListItemInfo}>
+              <span>Tel: (12345678901)</span>
+              <span>23 St. Revenue Avenue Istanbul</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div className={styles.pagination}>
+        <Pagination />
+      </div>
+    </section>
+  );
+};
+
+export default Datagrid;

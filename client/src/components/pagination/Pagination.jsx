665b0186ff566d6ae88925dd4847c5da309776dd
hasanarpat <black_the_dragon@hotmail.com>
2024-05-13 18:03:59 +0300

    finish pagination structure

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -1,7 +1,96 @@
+import { useSearchParams } from 'react-router-dom';
 import styles from './pagination.module.css';
 
 const Pagination = () => {
-  return <div className={styles.pagination}>Pagination</div>;
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get('page');
+  return (
+    <div className={styles.pagination}>
+      <div className={styles.paginationContainer}>
+        <div className={styles.paginationWrapper}>
+          <button disabled={page === null}>
+            <svg
+              stroke='currentColor'
+              fill='currentColor'
+              strokeWidth='0'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path fill='none' d='M0 0h24v24H0z'></path>
+              <path d='M6 6h2v12H6zm3.5 6 8.5 6V6z'></path>
+            </svg>
+          </button>
+          <ul className={styles.pageList}>
+            <li className={styles.page}>
+              <button
+                style={{
+                  backgroundColor: page === null && '#379cb4',
+                  borderColor: page === null && '#379cb4',
+                }}
+              >
+                1
+              </button>
+            </li>
+            <li className={styles.page}>
+              <button
+                style={{
+                  backgroundColor: parseInt(page) === 2 && '#379cb4',
+                  borderColor: parseInt(page) === 2 && '#379cb4',
+                }}
+              >
+                2
+              </button>
+            </li>
+            <li className={styles.page}>
+              <button
+                style={{
+                  backgroundColor: parseInt(page) === 3 && '#379cb4',
+                  borderColor: parseInt(page) === 3 && '#379cb4',
+                }}
+              >
+                3
+              </button>
+            </li>
+            <li className={styles.page}>
+              <button
+                style={{
+                  backgroundColor: parseInt(page) === 4 && '#379cb4',
+                  borderColor: parseInt(page) === 4 && '#379cb4',
+                }}
+              >
+                4
+              </button>
+            </li>
+            <li className={styles.page}>
+              <button
+                style={{
+                  backgroundColor: parseInt(page) === 5 && '#379cb4',
+                  borderColor: parseInt(page) === 5 && '#379cb4',
+                }}
+              >
+                5
+              </button>
+            </li>
+            <li className={styles.page}>
+              <button>...</button>
+            </li>
+          </ul>
+          <button disabled={page === null}>
+            <svg
+              stroke='currentColor'
+              fill='currentColor'
+              strokeWidth='0'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path fill='none' d='M0 0h24v24H0z'></path>
+              <path d='m6 18 8.5-6L6 6v12zM16 6v12h2V6h-2z'></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Pagination;
--- client/src/routes/arenas/Arenas.jsx
+++ client/src/routes/arenas/Arenas.jsx
@@ -919,7 +919,49 @@ const Arenas = () => {
                   <p>
                     Consequat laboris est occaecat ipsum nulla enim esse ad
                     magna labore exercitation qui Lorem deserunt. Reprehenderit
-                    nisi.
+                    nisi.In commodo duis ullamco laborum ullamco ad. Aliqua
+                    Lorem proident qui ut adipisicing irure eiusmod. Aute
+                    deserunt qui ullamco mollit occaecat incididunt ipsum labore
+                    enim ad. Reprehenderit sunt quis adipisicing occaecat irure.
+                    Dolore magna laboris nircitation qui Lorem deserunt.
+                    Reprehenderit nisi.In commodo duis ullamco laborum ullamco
+                    ad. Aliqua Lorem proident qui ut adipisicing irure eiusmod.
+                    Aute deserunt qui ullamco mollit occaecat incididunt ipsum
+                    labore enim ad. Reprehenderit sunt quis adipisicing occaecat
+                    irure. Dolore magna laboris nircitation qui Lorem deserunt.
+                    Reprehenderit nisi.In commodo duis ullamco laborum ullamco
+                    ad. Aliqua Lorem proident qui ut adipisicing irure eiusmod.
+                    Aute deserunt qui ullamco mollit occaecat incididunt ipsum
+                    labore enim ad. Reprehenderit sunt quis adipisicing occaecat
+                    irure. Dolore magna laboris nisi aliqua ipsum esse sit.
+                  </p>
+                  <div className={styles.allArenasListItemInfo}>
+                    <span>Tel: (12345678901)</span>
+                    <span>23 St. Revenue Avenue Istanbul</span>
+                  </div>
+                </div>
+              </Link>
+              <Link to={`/blabla`} className={styles.allArenasListItem}>
+                <img
+                  src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                  alt='arena rock'
+                />
+                <div className={styles.allArenasListItemTexts}>
+                  <h4>Arena Rock</h4>
+                  <p>
+                    Consequat laboris est occaecat ipsum nulla enim esse ad
+                    magna labore exercitrcitation qui Lorem deserunt.
+                    Reprehenderit nisi.In commodo duis ullamco laborum ullamco
+                    ad. Aliqua Lorem proident qui ut adipisicing irure eiusmod.
+                    Aute deserunt qui ullamco mollit occaecat incididunt ipsum
+                    labore enim ad. Reprehenderit sunt quis adipisicing occaecat
+                    irure. Dolore magna laboris nircitation qui Lorem deserunt.
+                    Reprehenderit nisi.In commodo duis ullamco laborum ullamco
+                    ad. Aliqua Lorem proident qui ut adipisicing irure eiusmod.
+                    Aute deserunt qui ullamco mollit occaecat incididunt ipsum
+                    labore enim ad. Reprehenderit sunt quis adipisicing occaecat
+                    irure. Dolore magna laboris niation qui Lorem deserunt.
+                    Reprehenderit nisi.
                   </p>
                   <div className={styles.allArenasListItemInfo}>
                     <span>Tel: (12345678901)</span>
@@ -953,27 +995,14 @@ const Arenas = () => {
                 <div className={styles.allArenasListItemTexts}>
                   <h4>Arena Rock</h4>
                   <p>
-                    Consequat laboris est occaecat ipsum nulla enim esse ad
-                    magna labore exercitation qui Lorem deserunt. Reprehenderit
-                    nisi.
-                  </p>
-                  <div className={styles.allArenasListItemInfo}>
-                    <span>Tel: (12345678901)</span>
-                    <span>23 St. Revenue Avenue Istanbul</span>
-                  </div>
-                </div>
-              </Link>
-              <Link to={`/blabla`} className={styles.allArenasListItem}>
-                <img
-                  src='https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-                  alt='arena rock'
-                />
-                <div className={styles.allArenasListItemTexts}>
-                  <h4>Arena Rock</h4>
-                  <p>
-                    Consequat laboris est occaecat ipsum nulla enim esse ad
-                    magna labore exercitation qui Lorem deserunt. Reprehenderit
-                    nisi.
+                    Consequat laboris ercitation qui Lorem deserunt.
+                    Reprehenderit nisi.In commodo duis ullamco laborum ullamco
+                    ad. Aliqua Lorem proident qui ut adipisicing irure eiusmod.
+                    Aute deserunt qui ullamco mollit occaecat incididunt ipsum
+                    labore enim ad. Reprehenderit sunt quis adipisicing occaecat
+                    irure. Dolore magna laboris nist occaecat ipsum nulla enim
+                    esse ad magna labore exercitation qui Lorem deserunt.
+                    Reprehenderit nisi.
                   </p>
                   <div className={styles.allArenasListItemInfo}>
                     <span>Tel: (12345678901)</span>

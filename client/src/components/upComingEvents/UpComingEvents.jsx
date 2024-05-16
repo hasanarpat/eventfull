9b7722c58bdd94965615436de159559ac7926bfb
hasanarpat <black_the_dragon@hotmail.com>
2024-05-16 16:17:22 +0300

    obtain eventlist as a new component

---
+++ client/src/components/upComingEvents/UpComingEvents.jsx
@@ -0,0 +1,205 @@
+import styles from './upcomingEvents.module.css';
+
+const UpComingEvents = () => {
+  return (
+    <section className={styles.eventList}>
+      <div className={styles.left}>
+        <div className={styles.eventDate}>
+          <span>13</span>
+          <span>JUN</span>
+        </div>
+        <div className={styles.eventDate}>
+          <span>19</span>
+          <span>JUN</span>
+        </div>
+        <div className={styles.eventDate}>
+          <span>06</span>
+          <span>JUL</span>
+        </div>
+        <div className={styles.eventDate}>
+          <span>06</span>
+          <span>JUL</span>
+        </div>
+        <div className={styles.eventDate}>
+          <span>06</span>
+          <span>JUL</span>
+        </div>
+      </div>
+      <div className={styles.center}>
+        <div className={styles.eventInfo}>
+          <h4>Voice of Darkness</h4>
+          <div className={styles.eventAddress}>
+            <div className={styles.eventArena}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 384 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z'></path>
+                </svg>
+              </span>
+              <span>45 Synytagna Square, in Athens</span>
+            </div>
+            <div className={styles.eventTime}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 512 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z'></path>
+                </svg>
+              </span>
+              <span>20:00 am</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.eventInfo}>
+          <h4>Voice of Darkness</h4>
+          <div className={styles.eventAddress}>
+            <div className={styles.eventArena}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 384 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z'></path>
+                </svg>
+              </span>
+              <span>45 Synytagna Square, in Athens</span>
+            </div>
+            <div className={styles.eventTime}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 512 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z'></path>
+                </svg>
+              </span>
+              <span>20:00 am</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.eventInfo}>
+          <h4>Voice of Darkness</h4>
+          <div className={styles.eventAddress}>
+            <div className={styles.eventArena}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 384 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z'></path>
+                </svg>
+              </span>
+              <span>45 Synytagna Square, in Athens</span>
+            </div>
+            <div className={styles.eventTime}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 512 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z'></path>
+                </svg>
+              </span>
+              <span>20:00 am</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.eventInfo}>
+          <h4>Voice of Darkness</h4>
+          <div className={styles.eventAddress}>
+            <div className={styles.eventArena}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 384 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z'></path>
+                </svg>
+              </span>
+              <span>45 Synytagna Square, in Athens</span>
+            </div>
+            <div className={styles.eventTime}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 512 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z'></path>
+                </svg>
+              </span>
+              <span>20:00 am</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.eventInfo}>
+          <h4>Voice of Darkness</h4>
+          <div className={styles.eventAddress}>
+            <div className={styles.eventArena}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 384 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z'></path>
+                </svg>
+              </span>
+              <span>45 Synytagna Square, in Athens</span>
+            </div>
+            <div className={styles.eventTime}>
+              <span>
+                <svg
+                  stroke='currentColor'
+                  fill='currentColor'
+                  strokeWidth='0'
+                  viewBox='0 0 512 512'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path d='M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z'></path>
+                </svg>
+              </span>
+              <span>20:00 am</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <img
+          src='https://images.unsplash.com/photo-1622886083536-67ff308dbe14?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt=''
+        />
+      </div>
+    </section>
+  );
+};
+
+export default UpComingEvents;

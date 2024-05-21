88c619a375d1899396d0b80c0aec89d651c6704f
hasanarpat <black_the_dragon@hotmail.com>
2024-05-21 18:44:41 +0300

    add upcoming events to artist page

--- client/src/routes/singleArena/SingleArena.jsx
+++ client/src/routes/singleArena/SingleArena.jsx
@@ -8,6 +8,7 @@ import Map from '../../components/map/Map';
 import SmartParagraph from '../../components/smartParagraph/SmartParagraph';
 import { mapItems } from '../../data/mock-map';
 import WriteComment from '../../components/writeComment/WriteComment';
+import Rate from '../../components/rate/Rate';
 
 const SingleArena = () => {
   const [showGallery, setShowGallery] = useState(false);
@@ -72,45 +73,7 @@ const SingleArena = () => {
                   type='arena'
                 />
                 <p>3204 St. Revenue Avenue Gazhane Istanbul</p>
-                <div className={styles.arenaRate}>
-                  {' '}
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    strokeWidth='0'
-                    viewBox='0 0 576 512'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'></path>
-                  </svg>{' '}
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    strokeWidth='0'
-                    viewBox='0 0 576 512'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'></path>
-                  </svg>{' '}
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    strokeWidth='0'
-                    viewBox='0 0 576 512'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'></path>
-                  </svg>{' '}
-                  <svg
-                    stroke='currentColor'
-                    fill='currentColor'
-                    strokeWidth='0'
-                    viewBox='0 0 576 512'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path d='M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z'></path>
-                  </svg>
-                </div>
+                <Rate />
               </div>
               <div className={styles.centerTopRight}>
                 <Map items={[mapItems[3]]} />
--- client/src/routes/singleArtist/SingleArtist.jsx
+++ client/src/routes/singleArtist/SingleArtist.jsx
@@ -6,6 +6,7 @@ import WriteComment from '../../components/writeComment/WriteComment';
 import Comment from '../../components/comment/Comment';
 import SmartParagraph from '../../components/smartParagraph/SmartParagraph';
 import Rate from '../../components/rate/Rate';
+import { Link } from 'react-router-dom';
 
 const SingleArtist = () => {
   const [showGallery, setShowGallery] = useState(false);
@@ -84,7 +85,113 @@ Elit ad ex nostrud ex. In esse qui veniam quis enim aliqua consectetur velit. An
                 </svg>
               </div>
             </div>
-            <div className={styles.centerBottom}>Upcoming Events</div>
+            <div className={styles.centerBottom}>
+              <h5>Upcoming Events</h5>
+              <div className={styles.upcomingEvents}>
+                <div className={styles.upcomingEvent}>
+                  <img
+                    src='https://plus.unsplash.com/premium_photo-1664302644902-7bf58fa20ef9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uY2VydCUyMHN0YWdlfGVufDB8fDB8fHww'
+                    alt=''
+                    className={styles.upcomingMainImg}
+                  />
+                  <p>
+                    <svg
+                      stroke='currentColor'
+                      fill='currentColor'
+                      strokeWidth='0'
+                      viewBox='0 0 24 24'
+                      height='1em'
+                      width='1em'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path fill='none' d='M0 0h24v24H0z'></path>
+                      <path d='M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z'></path>
+                      <path d='M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z'></path>
+                    </svg>
+                    Date & Time
+                  </p>
+                  <p>Wednesday 28th February at 15:00</p>
+                  <div className={styles.eventDetail}>
+                    <img
+                      src='https://plus.unsplash.com/premium_photo-1664302644902-7bf58fa20ef9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uY2VydCUyMHN0YWdlfGVufDB8fDB8fHww'
+                      alt=''
+                    />
+                    <h5>Arena Stadium</h5>
+                  </div>
+                  <Link to={`/blabla`}>
+                    <button>View Full Details</button>
+                  </Link>
+                </div>
+                <div className={styles.upcomingEvent}>
+                  <img
+                    src='https://plus.unsplash.com/premium_photo-1664302644902-7bf58fa20ef9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uY2VydCUyMHN0YWdlfGVufDB8fDB8fHww'
+                    alt=''
+                    className={styles.upcomingMainImg}
+                  />
+                  <p>
+                    <svg
+                      stroke='currentColor'
+                      fill='currentColor'
+                      strokeWidth='0'
+                      viewBox='0 0 24 24'
+                      height='1em'
+                      width='1em'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path fill='none' d='M0 0h24v24H0z'></path>
+                      <path d='M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z'></path>
+                      <path d='M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z'></path>
+                    </svg>
+                    Date & Time
+                  </p>
+                  <p>Wednesday 28th February at 15:00</p>
+                  <div className={styles.eventDetail}>
+                    <img
+                      src='https://plus.unsplash.com/premium_photo-1664302644902-7bf58fa20ef9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uY2VydCUyMHN0YWdlfGVufDB8fDB8fHww'
+                      alt=''
+                    />
+                    <h5>Arena Stadium</h5>
+                  </div>
+                  <Link to={`/blabla`}>
+                    <button>View Full Details</button>
+                  </Link>
+                </div>
+                <div className={styles.upcomingEvent}>
+                  <img
+                    src='https://plus.unsplash.com/premium_photo-1664302644902-7bf58fa20ef9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uY2VydCUyMHN0YWdlfGVufDB8fDB8fHww'
+                    alt=''
+                    className={styles.upcomingMainImg}
+                  />
+                  <p>
+                    <svg
+                      stroke='currentColor'
+                      fill='currentColor'
+                      strokeWidth='0'
+                      viewBox='0 0 24 24'
+                      height='1em'
+                      width='1em'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path fill='none' d='M0 0h24v24H0z'></path>
+                      <path d='M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z'></path>
+                      <path d='M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z'></path>
+                    </svg>
+                    Date & Time
+                  </p>
+                  <p>Wednesday 28th February at 15:00</p>
+                  <div className={styles.eventDetail}>
+                    <img
+                      src='https://plus.unsplash.com/premium_photo-1664302644902-7bf58fa20ef9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29uY2VydCUyMHN0YWdlfGVufDB8fDB8fHww'
+                      alt=''
+                    />
+                    <h5>Arena Stadium</h5>
+                  </div>
+                  <Link to={`/blabla`}>
+                    <button>View Full Details</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
           <div className={styles.top}>
             <Gallery setShowGallery={setShowGallery} />

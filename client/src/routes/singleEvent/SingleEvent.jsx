27141c37c69b7c4c53c9005372f2918fe32da591
hasanarpat <black_the_dragon@hotmail.com>
2024-05-23 18:19:16 +0300

    create page strcuture

--- client/src/routes/singleEvent/SingleEvent.jsx
+++ client/src/routes/singleEvent/SingleEvent.jsx
@@ -1,7 +1,19 @@
 import styles from './singleEvent.module.css';
 
 const SingleEvent = () => {
-  return <section className={styles.singleEvent}>SingleEvent</section>;
+  return (
+    <section className={styles.singleEvent}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.left}>
+            <div className={styles.top}></div>
+            <div className={styles.bottom}></div>
+          </div>
+          <div className={styles.right}></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SingleEvent;

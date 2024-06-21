2bbfdff9465b85a422a7af8a40e6c0f6011a70fd
hasanarpat <black_the_dragon@hotmail.com>
2024-06-21 21:27:44 +0300

    add prices, arena and artist forms to page

--- client/src/routes/newPost/NewPost.jsx
+++ client/src/routes/newPost/NewPost.jsx
@@ -1,5 +1,5 @@
-import styles from "./newPost.module.css";
-import { useState } from "react";
+import styles from './newPost.module.css';
+import { useState } from 'react';
 
 const NewPost = () => {
   const forms = [
@@ -31,10 +31,10 @@ const NewPost = () => {
       </h2>
     ),
   ];
-  const [formIndex, setFormIndex] = useState(0);
+  const [formIndex, setFormIndex] = useState(3);
 
   const handleNav = () => {
-    console.log("handle nav");
+    console.log('handle nav');
 
     // TODO set form index to navigate between forms
     // ! do not forget to check if form is completed before pass to next form
@@ -85,74 +85,115 @@ const NewPost = () => {
 };
 
 const DetailsForm = () => (
-  <form onSubmit="" id="details">
+  <form onSubmit='' id='details'>
     <div className={styles.formItem}>
-      <label htmlFor="eventName">Event Name</label>
-      <input type="text" id="eventName" placeholder="TNK Rock Fest" />
+      <label htmlFor='eventName'>Event Name</label>
+      <input type='text' id='eventName' placeholder='TNK Rock Fest' />
     </div>
     <div className={styles.formItem}>
-      <label htmlFor="eventDesc">Event Description</label>
-      <textarea type="text" id="eventDesc"></textarea>
+      <label htmlFor='eventDesc'>Event Description</label>
+      <textarea type='text' id='eventDesc'></textarea>
     </div>
     <div className={styles.formItem}>
-      <label htmlFor="eventDate">Event Date</label>
-      <input type="date" id="eventDate" />
+      <label htmlFor='eventDate'>Event Date</label>
+      <input type='date' id='eventDate' />
     </div>
     <div className={styles.formItem}>
-      <label htmlFor="eventTime">Event Times - Hours</label>
-      <input type="number" placeholder="start time" id="eventTime" min={1} />
-      <input type="number" placeholder="finish time" id="eventTime" min={1} />
+      <label htmlFor='eventTime'>Event Times - Hours</label>
+      <input type='number' placeholder='start time' id='eventTime' min={1} />
+      <input type='number' placeholder='finish time' id='eventTime' min={1} />
     </div>
     <div className={styles.formItem}>
-      <label htmlFor="eventFile">Event Images</label>
-      <input type="file" id="eventFile" />
+      <label htmlFor='eventCategory'>Event Category</label>
+      <select name='eventCategory' id='eventCategory'>
+        <option value='music'>Music</option>
+        <option value='health'>Health</option>
+        <option value='business'>Business</option>
+      </select>
+    </div>
+    <div className={styles.formItem}>
+      <label htmlFor='eventFile'>Event Images</label>
+      <input type='file' id='eventFile' />
     </div>
     <div className={styles.eventImages}>
       <div className={styles.img}>
         <img
-          src="https://images.unsplash.com/photo-1718792679559-5cfd607bb564?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
+          src='https://images.unsplash.com/photo-1718792679559-5cfd607bb564?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D'
+          alt=''
         />
         <button>X</button>
       </div>
       <div className={styles.img}>
         <img
-          src="https://images.unsplash.com/photo-1718792679559-5cfd607bb564?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
+          src='https://images.unsplash.com/photo-1718792679559-5cfd607bb564?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D'
+          alt=''
         />
         <button>X</button>
       </div>
     </div>
     <div className={styles.formItem}>
-      <label htmlFor="eventAddress">Event Address</label>
+      <label htmlFor='eventAddress'>Event Address</label>
       <input
-        type="text"
-        placeholder="St. Joseph Str. No 12"
-        id="eventAddress"
+        type='text'
+        placeholder='St. Joseph Str. No 12'
+        id='eventAddress'
       />
     </div>
     <div className={styles.formItem}>
-      <label htmlFor="eventCity">Event City</label>
-      <input type="text" placeholder="Moscow" id="eventCity" />
+      <label htmlFor='eventCity'>Event City</label>
+      <input type='text' placeholder='Moscow' id='eventCity' />
     </div>
   </form>
 );
 
 const ArtistForm = () => (
-  <form onSubmit="" id="artist">
-    artistForm
+  <form onSubmit='' id='artist'>
+    <div className={styles.formItem}>
+      <label htmlFor='artist'>Select Artist for your event</label>
+      <select name='artist' id='artist'>
+        <option value='Hayko Cepkin'>Hayko Cepkin</option>
+        <option value='Şebnem Ferah'>Şebnem Ferah</option>
+        <option value='Korn'>Korn</option>
+      </select>
+    </div>
   </form>
 );
 
 const ArenaForm = () => (
-  <form onSubmit="" id="arena">
-    arenaForm
+  <form onSubmit='' id='arena'>
+    <div className={styles.formItem}>
+      <label htmlFor='arena'>Select Arena for your event</label>
+      <select name='arena' id='arena'>
+        <option value='Arena Rock'>Arena Rock</option>
+        <option value='Horn Bar'>Horn Bar</option>
+        <option value='Queen Rock'>Queen Rock</option>
+      </select>
+    </div>
   </form>
 );
 
 const PricesAndSeatsForm = () => (
-  <form onSubmit="" id="pricesAndSeats">
-    pricesAndSeatsForm
+  <form onSubmit='' id='pricesAndSeats'>
+    <div className={styles.formItem}>
+      <label htmlFor='eventPrice'>Event Price</label>
+      <input
+        type='number'
+        placeholder='$100'
+        id='eventPrice'
+        min={0}
+        max={10000}
+      />
+    </div>
+    <div className={styles.formItem}>
+      <label htmlFor='eventSeatCount'>Seat Count</label>
+      <input
+        type='number'
+        placeholder='How many seat do you have'
+        id='eventSeatCount'
+        min={10}
+        max={100000}
+      />
+    </div>
   </form>
 );
 
